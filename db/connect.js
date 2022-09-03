10dfb1ce66806715408410aad3b523ac7d8d5878
Rahul Mallick <rahulmallick1115@gmail.com>
2022-09-03 13:47:10 +0530

    modified

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 
-const connectionString =
-  "mongodb+srv://admin:rahulAdmin@nodeexpressproject.0muk7k5.mongodb.net/task-manager?retryWrites=true&w=majority";
-
 const connectDB = (url) => {
-  return mongoose.connect(connectionString);
+  return mongoose.connect(url);
 };
 
 module.exports = connectDB;
